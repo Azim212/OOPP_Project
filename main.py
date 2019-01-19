da94d0e2ef89ff409a1863dda2847e3c5a9ea786
Azim212 <183574a@mymail.nyp.edu.sg>
2019-01-19 20:15:37 +0800

    ACTUAL WORKING VALIDATION FINALLY

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from simData import *
 import shelve
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iufnaofiLKE'
-
+#simStorage = shelve.open('simStorage')
+#calc.led.data = simStorage['ledNum']
+#calc.cfl.data = simStorage['cflNum']
+#calc.inc.data = simStorage['incNum']
+#calc.toish.data = simStorage['toiletNum']
+#calc.toitype.data = simStorage['toiletType']
+#simStorage.close()
 
 @app.route('/')
 def index():
@@ -13,17 +19,15 @@ def index():
 @app.route('/Sim.html', methods=['GET', 'POST'])
 def sim():
     calc = simData(request.form)
-    if request.method == 'POST' and calc.validate():
-        return render_template("SimResults.html", calc=calc)
+    error = None
+    if request.method == 'POST':
+        if calc.validate():
+            return render_template("SimResults.html", calc=calc)
+        else:
+            error = 'Please enter numbers only.'
+            return render_template("Sim.html", calc=calc, error=error)
     else:
-        simStorage = shelve.open('simStorage')
-        calc.led.data = simStorage['ledNum']
-        calc.cfl.data = simStorage['cflNum']
-        calc.inc.data = simStorage['incNum']
-        calc.toish.data = simStorage['toiletNum']
-        calc.toitype.data = simStorage['toiletType']
-        simStorage.close()
-        return render_template("Sim.html", calc=calc)
+        return render_template("Sim.html", calc=calc, error=error)
 
 
 if __name__ == '__main__':
--- simData.py
+++ simData.py
@@ -2,10 +2,10 @@ from wtforms import SubmitField, StringField, SelectField, Form, validators
 
 
 class simData(Form):
-    led = StringField('LED', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired(message='Please enter a number')])
-    cfl = StringField('CFL', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired(message='Please enter a number')])
-    inc = StringField('Incandescent', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired(message='Please enter a number')])
-    toish = StringField('How many bathrooms do you have?', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired(message='Please enter a number')])
+    led = StringField('LED', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired()])
+    cfl = StringField('CFL', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired()])
+    inc = StringField('Incandescent', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired()])
+    toish = StringField('How many bathrooms do you have?', [validators.Regexp(regex="^\d+$", message='Please enter a number'), validators.DataRequired()])
     toitype = SelectField('What type of toilets do you use?', choices=[('Old', 'Old'), ('Ultra Low Flush', 'Ultra Low Flush'), ('High Efficiency', 'High Efficiency'), ('Dual Flush', 'Dual Flush')])
     submit = SubmitField('Submit')
 
